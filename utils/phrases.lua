42e9f4623ccdfdaa619cbdd394f82c1683577e28
alex9 <66374743+piglex9@users.noreply.github.com>
2022-10-02 04:22:40 +0200

    Update phrases.lua

--- utils/phrases.lua
+++ utils/phrases.lua
@@ -6,12 +6,9 @@ local phrases = {
     "шкила безскиловая";
     "если-бы использовал ice tray, тебя-бы не арестовали";
     "👍INFINITE MONEY FREE HACK (NO VIRUS👍 NO SCAM 👍 TRUSTED 2022) JOIN: - DIS . GG /5Eqt6QBfYY";
-    "бот";
     "мать в канаве";
     "🤑UNLIMITED MONEY 🤑 FREE HACK JOIN: DIS . GG /5Eqt6QBfYY";
     "HACK JAILBREAK FREE 2022 😎 NO VIRUS 👍 NO BAN 👍 JOIN: - DIS . GG /5Eqt6QBfYY";
-    "безмамный";
-    "поной";
     "FREE HACK JOIN (NO BAN): - DIS . GG /5Eqt6QBfYY";
     "👍👍FREE HACK (NO VIRUS👍 TRUSTED 2022) JOIN: - DIS . GG /5Eqt6QBfYY";
     "наверни смачного поноса, пока плачешь";
@@ -23,15 +20,13 @@ local phrases = {
     "👍INFINITE MONEY FREE HACK (NO VIRUS👍 TRUSTED 2022) JOIN: - DIS . GG /5Eqt6QBfYY";
     "FREE HACK JOIN (NO BAN 👍): - DIS . GG /5Eqt6QBfYY";
     "видимо когда ты хотел поиграть, у отчима были другие планы";
-    "ахахахаха нубик";
     "дед инсайд с чёлкой до китая 🤡";
-    "ICE TRAY BEST NEW HACK 2022 UNDETECTED 👽 JOIN: - DIS . GG /5Eqt6QBfYY"; 
+    "ICE TRAY BEST NEW HACK UNDETECTED 👽 JOIN: - DIS . GG /5Eqt6QBfYY"; 
     "слишком изично с ice tray";
     "ICE TRAY FREE HACK UNDETECTED 👀 JOIN: - DIS . GG /5Eqt6QBfYY";
     "я твою маму на шоссе купил";
     "JOIN ICE TRAY NOW - DIS . GG /5Eqt6QBfYY";
     "🧊 ice tray, запомни это имя бомжара";
-    "БАРБОСКИНЫ ТИМА! БАРБОСКИНЫ ТИМА! БАРБОСКИНЫ ТИМА! БАРБОСКИНЫ ТИМА!";
 }
 
 return phrases
