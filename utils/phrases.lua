a2580fcf8db59cc1eb028d5640bef5e7bb20a540
alex9 <66374743+piglex9@users.noreply.github.com>
2022-10-23 01:43:35 +0200

    Update phrases.lua

--- utils/phrases.lua
+++ utils/phrases.lua
@@ -1,33 +1,33 @@
 local phrases = {
-    "alex9 gave me hack free JOIN: - DIS . GG /icetray";
-    "🤑 JOIN ICE TRAY NOW - DIS . GG /icetray";
-    "SPEED HACK ICE TRAY JOIN😊 - DIS . GG /icetray";
+    "alex9 gave me hack free JOIN: - DIS . GG /5Eqt6QBfYY";
+    "🤑 JOIN ICE TRAY NOW - DIS . GG /5Eqt6QBfYY";
+    "SPEED HACK ICE TRAY JOIN😊 - DIS . GG /5Eqt6QBfYY";
     "соси молча";
     "alex9 love u";
     "ахахахаха нубик 🤣";
     "шкила безскиловая";
     "если-бы использовал ice tray, тебя-бы не арестовали";
-    "👍INFINITE MONEY FREE HACK (NO VIRUS👍 NO SCAM 👍 TRUSTED 2022) JOIN: - DIS . GG /icetray";
+    "👍INFINITE MONEY FREE HACK (NO VIRUS👍 NO SCAM 👍 TRUSTED 2022) JOIN: - DIS . GG /5Eqt6QBfYY";
     "мать в канаве";
-    "🤑UNLIMITED MONEY 🤑 FREE HACK JOIN: DIS . GG /icetray";
-    "HACK JAILBREAK FREE 2022 😎 NO VIRUS 👍 NO BAN 👍 JOIN: - DIS . GG /icetray";
-    "FREE HACK JOIN (NO BAN): - DIS . GG /icetray";
-    "👍👍FREE HACK (NO VIRUS👍 TRUSTED 2022) JOIN: - DIS . GG /icetray";
+    "🤑UNLIMITED MONEY 🤑 FREE HACK JOIN: DIS . GG /5Eqt6QBfYY";
+    "HACK JAILBREAK FREE 2022 😎 NO VIRUS 👍 NO BAN 👍 JOIN: - DIS . GG /5Eqt6QBfYY";
+    "FREE HACK JOIN (NO BAN): - DIS . GG /5Eqt6QBfYY";
+    "👍👍FREE HACK (NO VIRUS👍 TRUSTED 2022) JOIN: - DIS . GG /5Eqt6QBfYY";
     "наверни смачного поноса, пока плачешь";
     "ice tray, запомни это имя бомжара";
-    "🤑UNLIMITED MONEY FREE HACK JOIN: DIS . GG /icetray";
+    "🤑UNLIMITED MONEY FREE HACK JOIN: DIS . GG /5Eqt6QBfYY";
     "пососи ❤️";
     "если-бы использовал ice tray, тебя-бы не арестовали 🧊";
     "нравится, когда без вазелина?";
-    "👍INFINITE MONEY FREE HACK (NO VIRUS👍 TRUSTED 2022) JOIN: - DIS . GG /icetray";
-    "FREE HACK JOIN (NO BAN 👍): - DIS . GG /icetray";
+    "👍INFINITE MONEY FREE HACK (NO VIRUS👍 TRUSTED 2022) JOIN: - DIS . GG /5Eqt6QBfYY";
+    "FREE HACK JOIN (NO BAN 👍): - DIS . GG /5Eqt6QBfYY";
     "видимо когда ты хотел поиграть, у отчима были другие планы";
     "дед инсайд с чёлкой до китая 🤡";
-    "ICE TRAY BEST NEW HACK UNDETECTED 👽 JOIN: - DIS . GG /icetray"; 
+    "ICE TRAY BEST NEW HACK UNDETECTED 👽 JOIN: - DIS . GG /5Eqt6QBfYY"; 
     "слишком изично с ice tray";
-    "ICE TRAY FREE HACK UNDETECTED 👀 JOIN: - DIS . GG /icetray";
+    "ICE TRAY FREE HACK UNDETECTED 👀 JOIN: - DIS . GG /5Eqt6QBfYY";
     "я твою маму на шоссе купил";
-    "JOIN ICE TRAY NOW - DIS . GG /icetray";
+    "JOIN ICE TRAY NOW - DIS . GG /5Eqt6QBfYY";
     "🧊 ice tray, запомни это имя бомжара";
 }
 
