173bbb62785974305841dbaabb45860967099ab1
alex9 <66374743+piglex9@users.noreply.github.com>
2022-11-23 06:47:22 +0100

    Create ambient.lua

---
+++ utils/ambient.lua
@@ -0,0 +1,27 @@
+local ambient = {}
+
+for i,v in next, getgc(true) do
+    if type(v) == "table" and rawget(v, "em") and not rawget(v, "GetVehiclePacket") then
+        for i2,v2 in next, v.em do
+            if type(v2) == "function" and islclosure(v2) then
+                local con = getconstants(v2)
+                if table.find(con, "Day") and table.find(con, "Night") then
+                    ambient.func = v2
+                end
+            end
+        end
+    end
+end
+
+function ambient.get()
+    return tostring(getupvalue(ambient.func, 1))
+end
+
+function ambient.set(value)
+    if value ~= "Day" and value ~= "Night" and value ~= "Storm" then
+        assert(value == nil)
+    end
+    return ambient.func(value)
+end
+
+return ambient
