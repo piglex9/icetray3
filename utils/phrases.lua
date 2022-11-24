b7b5c119257f10b877c10a38fb35de50cbb56d98
alex9 <66374743+piglex9@users.noreply.github.com>
2022-11-24 05:14:04 +0100

    Update phrases.lua

--- utils/phrases.lua
+++ utils/phrases.lua
@@ -1,8 +1,5 @@
 local phrases = {
-    "promovam tot ce tine de rasism , fasism , glume cu tenta sexuala , propaganda anti evrei si multe altele pe dis.gg/icetray";
     "become good w/ ice tray, join: DIS.GG/icetray";
-    "eu sunt boss, folosesc ice tray, tu esti un sclav";
-    "prea usor cu ice tray";
     "UNDETECTED HACK ICE TRAY FREE DOWNLOAD - DIS.GG/icetray";
     "🤑 JOIN ICE TRAY NOW - DIS . GG /icetray";
     "SPEED HACK ICE TRAY JOIN😊 - DIS . GG /icetray";
