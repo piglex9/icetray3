bc781e50bcd4a58a788b6c796ed81a2ef0c114ee
alex9 <66374743+piglex9@users.noreply.github.com>
2022-12-07 06:40:35 +0100

    Create get_disabler_status.lua

---
+++ utils/get_disabler_status.lua
@@ -0,0 +1,10 @@
+return function(tools, disabler)
+    if tools.registry.isHooked(disabler) then
+        return "🛑"
+    end
+    local disabler = tools.utils.synapse_functions.getupvalue(disabler,2)
+    if disabler == false then
+        return "❌"
+    end
+    return "✅"
+end
