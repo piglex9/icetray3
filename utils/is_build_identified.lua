f5759581b8875c31206d38b445df9ca4d6c4ffbb
alex9 <66374743+piglex9@users.noreply.github.com>
2022-12-07 06:34:23 +0100

    Create is_build_identified.lua

---
+++ utils/is_build_identified.lua
@@ -0,0 +1,10 @@
+return function(build)
+    if build == "synx" then
+        return "✅"
+    elseif build == "oxygen-api" then
+        return "✅"
+    else
+        return "❌"
+    end
+    return "❌"
+end
