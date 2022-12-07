0ca07dfbe8116b774760aeb870adca1d8efe8371
alex9 <66374743+piglex9@users.noreply.github.com>
2022-12-07 06:46:10 +0100

    Update ceasul.lua

--- utils/ceasul.lua
+++ utils/ceasul.lua
@@ -1,7 +1,8 @@
-return function(ceas, memorie)
+return function(tools, ceas, memorie)
     local c = os.clock()
     local iff = ceas - c
     ceas = c
+    tools.loss = iff
     for i=1, #memorie do
         local v = memorie[i]
         if v.i < c - v.t then
