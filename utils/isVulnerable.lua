37c9207116b538904406c6de59cf30913d28c045
alex9 <66374743+piglex9@users.noreply.github.com>
2023-02-11 13:30:47 +0100

    Create isVulnerable.lua

---
+++ utils/isVulnerable.lua
@@ -0,0 +1,17 @@
+return function(p1, p2)
+    if p1 == "Police" then
+        if p2 == "Criminal" then
+            return true
+        elseif p2 == "Prisoner" then
+            return true
+        end
+    elseif p1 == "Criminal" or p1 == "Prisoner" then
+        if p2 == "Police" then
+            return true
+        elseif p2 == "Criminal" then
+            return false
+        elseif p2 == "Prisoner" then
+            return false
+        end
+    end
+end
