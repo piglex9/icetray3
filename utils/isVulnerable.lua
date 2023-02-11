fccef7ffcdb3b8bf0df2d122cd77f229a371f2c7
alex9 <66374743+piglex9@users.noreply.github.com>
2023-02-11 13:35:04 +0100

    Update isVulnerable.lua

--- utils/isVulnerable.lua
+++ utils/isVulnerable.lua
@@ -8,10 +8,6 @@ return function(p1, p2)
     elseif p1 == "Criminal" or p1 == "Prisoner" then
         if p2 == "Police" then
             return true
-        elseif p2 == "Criminal" then
-            return false
-        elseif p2 == "Prisoner" then
-            return false
         end
     end
     return false
