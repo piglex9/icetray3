01aabc2edb3b4703492c22469d4bbcfe87ed757b
alex9 <66374743+piglex9@users.noreply.github.com>
2023-02-28 22:18:20 +0100

    Update phrases.lua

--- utils/phrases.lua
+++ utils/phrases.lua
@@ -1,6 +1,5 @@
 local phrases = {
     "asta se intampla daca nu folosesti icetray.lua";
-    "esti un negru";
     "hahahaha slabule 🤣";
     "doar prosti nu folosesc ice tray 🤡";
     "prea usor cu ice tray";
@@ -13,36 +12,4 @@ local phrases = {
     'print(string.rep("ez", 228))';
 }
 
-local phrases_new = {
-
-                    type1 = {
-                        "hungary pooron kid from bucket balaton tell me i dont food 😂😂😂";
-                        'i call gypsy kid "bozgor" and kid cry 😂😂😂';
-                        "1918";
-                    };
-                    type2 = {
-                        "orice bozgor merita omorat";
-                        "si romi..";
-                    };
-                    type3 = {
-                        "all dogs say recorded but me BLESSED by ice tray 😈";
-                        "software by ice tray coding corporation";
-                    };
-                    type4 = {
-                        "am ceva de spus";
-                        "SAMIBAGPULANBOZGORI";
-                    };
-                    type5 = {
-                        "a";
-                        "b";
-                        "t";
-                        true;
-                        "lalalal please shoot a bullet inside my head";
-                        "test";
-                        false;
-                        1;
-                        "fglasglsadglasslg";
-                    };
-}
-
-return phrases, phrases_new
+return phrases
