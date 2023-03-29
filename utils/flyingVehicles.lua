29c19fc0c2d23eaa18cb4da740b812b3e8514d0c
alex9 <66374743+piglex9@users.noreply.github.com>
2023-03-29 15:37:08 +0200

    Create flyingVehicles.lua

---
+++ utils/flyingVehicles.lua
@@ -0,0 +1,14 @@
+return {
+    Drone = true;
+    Heli = true;
+    BlackHawk = true;
+    UFO = true;
+    Celsior = true;
+    Jet = true;
+    Blade = true;
+    EscapeBot = true;
+    LittleBird = true;
+    CrewCapsule = true;
+    Lia = true;
+    Stunt = true;
+}
