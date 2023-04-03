391ecf6a7c6601053bfa3919d42fe1472cfa1388
alex9 <66374743+piglex9@users.noreply.github.com>
2023-04-03 19:11:56 +0200

    Create phrases.lua

---
+++ utils/phrases.lua
@@ -0,0 +1,15 @@
+local phrases = {
+    "asta se intampla daca nu folosesti icetray.lua";
+    "hahahaha slabule 🤣";
+    "doar prosti nu folosesc ice tray 🤡";
+    "prea usor cu ice tray";
+    "tava de gheata 🥶";
+    "bozgor";
+    "😎 agent spharangel kox 😎";
+    "prea usor pentru agent spharangel kox";
+    "ICE TRAY 55 - 0 ANTI HACK";
+    "icetray.lua > all";
+    'print(string.rep("ez", 228))';
+}
+
+return phrases
