828a37adf78f77360016ff94a9834ae76aa9b8d4
alex9 <66374743+piglex9@users.noreply.github.com>
2023-04-03 19:11:32 +0200

    Delete utils directory

--- utils/isArrested.lua
+++
@@ -1,6 +0,0 @@
-return function(player)
-    if player:FindFirstChild("Folder") and player.Folder:FindFirstChild("Cuffed") then
-        return true
-    end
-    return false
-end
--- utils/noUILoad.lua
+++
@@ -1,7 +0,0 @@
-return function()
-    global.noUILoad = true
-    if global.ui and global.ui.window.tab then
-        global.ui.window.tab:Destroy()
-    end
-    global.run = false
-end
