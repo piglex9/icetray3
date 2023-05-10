061a0e81d052e3a38bf76199c1f3cb4c38932a9a
alex9 <66374743+piglex9@users.noreply.github.com>
2023-05-10 16:26:07 +0200

    Update latest.lua

--- latest.lua
+++ latest.lua
@@ -1,1 +1,1 @@
-return game:GetService("Players").LocalPlayer:Kick("Join our discord and buy a ice tray v4 key here: https://discord.gg/icetray")
+return game:GetService("Players").LocalPlayer:Kick("Join our discord and buy a ice tray v4 key here: https://discord.gg/5Eqt6QBfYY")
